aac7184348e7982de2f6800a5bc4932eee024d34
Jose Lima <jalimag0206@gmail.com>
2023-12-24 22:40:29 -0300

    feat: add status colors

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -20,25 +20,31 @@ export function History() {
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há cerca 2 meses</td>
-                            <td>Concluído</td>
+                            <td>
+                                <S.Status statusColor="green">
+                                    Concluído
+                                </S.Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há cerca 2 meses</td>
-                            <td>Concluído</td>
+                            <td>
+                                <S.Status statusColor="yellow">
+                                    Em andamento
+                                </S.Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há cerca 2 meses</td>
-                            <td>Concluído</td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há cerca 2 meses</td>
-                            <td>Concluído</td>
+                            <td>
+                                <S.Status statusColor="red">
+                                    Interrompido
+                                </S.Status>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
