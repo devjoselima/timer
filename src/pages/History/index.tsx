08564eaa78cc903437ef58b19c0296113484c697
Jose Lima <jalimag0206@gmail.com>
2023-12-26 23:47:29 -0300

    feat: add format finished date

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,7 @@
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale/pt-BR'
+
 import { CyclesContext } from '../../contexts/CyclesContext'
 
 import * as S from './styles'
@@ -25,7 +28,12 @@ export function History() {
                                 <tr key={cycle.id}>
                                     <td>{cycle.task}</td>
                                     <td>{cycle.minutesAmount} minutos</td>
-                                    <td>{cycle.startDate.toISOString()}</td>
+                                    <td>
+                                        {formatDistanceToNow(cycle.startDate, {
+                                            addSuffix: true,
+                                            locale: ptBR,
+                                        })}
+                                    </td>
                                     <td>
                                         {cycle.finishedDate && (
                                             <S.Status statusColor="green">
