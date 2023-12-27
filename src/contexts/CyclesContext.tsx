24d598068dc5b7c5e966e31caffc55fdedff724e
Jose Lima <jalimag0206@gmail.com>
2023-12-27 02:53:26 -0300

    feat: ensure cycles are saving in localhost

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useReducer, useState } from 'react'
+import {
+    ReactNode,
+    createContext,
+    useEffect,
+    useReducer,
+    useState,
+} from 'react'
 import { Cycle, CyclesContextType, CreateCycleData } from '../interfaces/Cycle'
 import { cyclesReducer } from '../reducers/cycles/reducer'
 import {
@@ -6,6 +12,7 @@ import {
     interruptCurrentCycleAction,
     markCurrentCycleAsFinishedAction,
 } from '../reducers/cycles/action'
+import { differenceInSeconds } from 'date-fns'
 
 export const CyclesContext = createContext({} as CyclesContextType)
 
@@ -16,16 +23,43 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
     children,
 }: CyclesContextProviderProps) {
-    const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-        cycles: [],
-        activeCycleId: null,
+    const [cyclesState, dispatch] = useReducer(
+        cyclesReducer,
+        {
+            cycles: [],
+            activeCycleId: null,
+        },
+        (initialState) => {
+            const storedStateAsJSON = localStorage.getItem(
+                '@timer:cycles-state-1.0.0'
+            )
+
+            if (storedStateAsJSON) {
+                return JSON.parse(storedStateAsJSON)
+            }
+
+            return initialState
+        }
+    )
+    const { cycles, activeCycleId } = cyclesState
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+        if (activeCycle) {
+            return differenceInSeconds(
+                new Date(),
+                new Date(activeCycle.startDate)
+            )
+        }
+
+        return 0
     })
 
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+    useEffect(() => {
+        const stateJSON = JSON.stringify(cyclesState)
 
-    const { cycles, activeCycleId } = cyclesState
-
-    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+        localStorage.setItem('@timer:cycles-state-1.0.0', stateJSON)
+    }, [cyclesState])
 
     function setSecondsPassed(seconds: number) {
         setAmountSecondsPassed(seconds)
--- src/pages/Home/components/CountDown/index.tsx
+++ src/pages/Home/components/CountDown/index.tsx
@@ -23,7 +23,7 @@ export function CountDown() {
             interval = setInterval(() => {
                 const secondsDifference = differenceInSeconds(
                     new Date(),
-                    activeCycle.startDate
+                    new Date(activeCycle.startDate)
                 )
 
                 if (secondsDifference >= totalSeconds) {
